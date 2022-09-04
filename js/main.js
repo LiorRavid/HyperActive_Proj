6bc2fc9db55c67a9127d9918952652736fe72e41
Lior Ravid <simoplay@gmail.com>
2022-09-04 10:30:17 +0300

    added animations questions+public_services

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 'use strict'
 
-function onToggleQuestion(id){
+function onToggleQuestion(id,id2){
     const element = document.getElementById(`${id}`);
     element.classList.toggle("question-open");
+    const question = document.getElementById(`${id2}`);
+    const questionChild = question.lastElementChild;
+    if(questionChild.classList.contains('fa-circle-arrow-down')){
+        questionChild.classList.remove('fa-circle-arrow-down');
+        questionChild.classList.add('fa-circle-arrow-up');
+    }
+    else{
+        questionChild.classList.remove('fa-circle-arrow-up');
+        questionChild.classList.add('fa-circle-arrow-down');
+    }
 }
