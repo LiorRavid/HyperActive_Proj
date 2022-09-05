6fd486bf772ba59db1a741150b940b0535a633c7
Lior Ravid <simoplay@gmail.com>
2022-09-05 16:53:53 +0300

    added header responsivity

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 'use strict'
 
+function onInit(){
+    window.addEventListener('resize',(event)=>{
+        let windowWidth = event.target.innerWidth;
+        const navBar = document.getElementById('nav-bar');
+        if(windowWidth > 925){
+            if(navBar.classList.contains("hidden")){
+                navBar.classList.remove("hidden");
+            }
+        }
+        if(windowWidth < 925){
+            if(!navBar.classList.contains("hidden")){
+                navBar.classList.add("hidden");
+            }
+        }
+    })
+}
+
 function onToggleQuestion(id,id2){
     const element = document.getElementById(`${id}`);
     element.classList.toggle("question-open");
@@ -12,5 +29,54 @@ function onToggleQuestion(id,id2){
     else{
         questionChild.classList.remove('fa-circle-arrow-up');
         questionChild.classList.add('fa-circle-arrow-down');
+    }
+}
+
+function onOpenCategory(idDetails,idCategory){
+    const element = document.getElementById(`${idDetails}`);
+    element.classList.toggle("question-open");
+    const category = document.getElementById(`${idCategory}`);
+    const categoryChild = category.lastElementChild;
+    // categoryChild.classList.toggle("plus-to-x");
+    if(categoryChild.classList.contains('plus-to-x')){
+        categoryChild.classList.remove('plus-to-x');
+        categoryChild.classList.add('x-to-plus');
+    }
+    else if(categoryChild.classList.contains('x-to-plus')){
+        categoryChild.classList.remove('x-to-plus');
+        categoryChild.classList.add('plus-to-x');
+    }
+    else{
+        categoryChild.classList.add('plus-to-x');
+    }
+}
+
+function onHamburgerClick(){
+    const navBar = document.getElementById('nav-bar');
+    navBar.classList.toggle("hidden");
+    const hamburgerBtn1 = document.getElementById('bar1');
+    const hamburgerBtn2 = document.getElementById('bar2');
+    const hamburgerBtn3 = document.getElementById('bar3');
+    hamburgerBtn1.classList.add('bar1');
+    hamburgerBtn2.classList.add('bar2');
+    hamburgerBtn3.classList.add('bar3');
+}
+
+function onOpenCareers(){
+    const careerDetails = document.getElementById('careers__details');
+    careerDetails.classList.toggle("hidden");
+}
+
+function onOpenCareersDesktop(){
+    if(window.innerWidth > 925){
+        const careerDetails = document.getElementById('careers__details');
+        careerDetails.classList.remove("hidden");
+    }
+}
+
+function onCloseCareersDesktop(){
+    if(window.innerWidth > 925){
+        const careerDetails = document.getElementById('careers__details');
+        careerDetails.classList.add("hidden");
     }
 }
